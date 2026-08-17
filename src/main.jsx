18b68206848f4b0939b4aec4014264cc24ef6300
Ntsoaki <ntsoakisoai@gmail.com>
2026-08-17 06:18:43 +0200

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ import store from "./redux/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter basename="/paradise-nursery">
+      <App />
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
